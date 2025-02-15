e4d91109d74c1ccfcb33aa8769eef67d0d5f371c
ai-bits <guenter.huber22@gmail.com>
2025-02-15 19:36:13 +0100

    trying to complete the torch fragment

--- py/hello-torch1.py
+++ py/hello-torch1.py
@@ -2,7 +2,6 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
 import torch
 
 # Specify the local directory where the model files are stored
-#model_directory = "C:\\dl\\xLSTM-7b" #"NX-AI/xLSTM-7b" #HF Hub path
 model_directory = "/home/gy/dl/xLSTM-7b" #"NX-AI/xLSTM-7b" #HF Hub path
 
 # Set the device to cuda
@@ -19,7 +18,7 @@ xlstm = AutoModelForCausalLM.from_pretrained(model_directory, config=xlstm_confi
 from pprint import pprint
 pprint(xlstm.backbone.blocks[0].mlstm_layer.config)
 
-# from Triton v
+# Load the tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_directory)
 
 # Tokenize the input and move it to the same device
