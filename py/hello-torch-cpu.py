0404fde760f908d5478a60647f298201b7edb668
ai-bits <guenter.huber22@gmail.com>
2025-02-15 21:31:32 +0100

    hello-torch-cpu.py WORKING!!!

--- py/hello-torch-cpu.py
+++ py/hello-torch-cpu.py
@@ -4,28 +4,27 @@ import torch
 # Specify the local directory where the model files are stored
 model_directory = "/home/gy/dl/xLSTM-7b" #"NX-AI/xLSTM-7b" #HF Hub path
 
-# Set the device to cuda
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Set the device to cpu
+device = torch.device("cpu")
 
 xlstm_config = AutoConfig.from_pretrained(model_directory)
 xlstm_config.step_kernel = "native"
 xlstm_config.chunkwise_kernel = "chunkwise--native_autograd"
 xlstm_config.sequence_kernel = "native_sequence__native"
 
-xlstm = AutoModelForCausalLM.from_pretrained(model_directory, config=xlstm_config, device_map="auto")
+# Load the model and tokenizer on the CPU
+xlstm = AutoModelForCausalLM.from_pretrained(model_directory, config=xlstm_config).to(device)
+tokenizer = AutoTokenizer.from_pretrained(model_directory)
 
 # verify selected kernels
 from pprint import pprint
 pprint(xlstm.backbone.blocks[0].mlstm_layer.config)
 
-# Load the tokenizer
-tokenizer = AutoTokenizer.from_pretrained(model_directory)
-
 # Tokenize the input and move it to the same device
 tokens = tokenizer("Hello xLSTM, how are you doing?", return_tensors='pt')['input_ids'].to(device)
 
 # Generate output
-out = xlstm.module.generate(tokens, max_new_tokens=20)
+out = xlstm.generate(tokens, max_new_tokens=20)
 
 # Decode and print the output
 print(tokenizer.decode(out[0]))
--- py/hello-torch.py
+++ py/hello-torch.py
@@ -2,7 +2,6 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
 import torch
 
 # Specify the local directory where the model files are stored
-#model_directory = "C:\\dl\\xLSTM-7b" #"NX-AI/xLSTM-7b" #HF Hub path
 model_directory = "/home/gy/dl/xLSTM-7b" #"NX-AI/xLSTM-7b" #HF Hub path
 
 # Set the device to cuda
@@ -19,14 +18,23 @@ xlstm = AutoModelForCausalLM.from_pretrained(model_directory, config=xlstm_confi
 from pprint import pprint
 pprint(xlstm.backbone.blocks[0].mlstm_layer.config)
 
-# from Triton v
+# Wrap the model with DataParallel to use multiple GPUs
+if torch.cuda.device_count() > 1:
+    xlstm = torch.nn.DataParallel(xlstm)
+
+xlstm = xlstm.to(device)
+
+# Load the tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_directory)
 
 # Tokenize the input and move it to the same device
 tokens = tokenizer("Hello xLSTM, how are you doing?", return_tensors='pt')['input_ids'].to(device)
 
 # Generate output
-out = xlstm.module.generate(tokens, max_new_tokens=20)
+if torch.cuda.device_count() > 1:
+    out = xlstm.module.generate(tokens, max_new_tokens=20)
+else:
+    out = xlstm.generate(tokens, max_new_tokens=20)
 
 # Decode and print the output
 print(tokenizer.decode(out[0]))
